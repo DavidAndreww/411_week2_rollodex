e37fde3f7ff1598b092bbababa587304a9079b23
David Bille <d_bille@yahoo.com>
2020-01-11 16:03:48 -0600

    working on getting data

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,32 @@
 import React from "react";
-import UserCard from "./UserCard";
+// import UserCard from "./UserCard";
 
 class App extends React.Component {
   state = {
     users: []
-  }
-  
+  };
+
   fetchData = () => {
-    fetch('https://randomuser.me/api?results=25')
-    .then(response => response.json())
-    .then(parsedJSON => this.setState({users: parsedJSON}))
-    .catch(error => console.log('Error occured: ', error))
-  } 
+    fetch("https://randomuser.me/api?results=25")
+      .then(response => response.json())
+      .then(parsedJSON => this.setState({ users: parsedJSON }))
+      .catch(error => console.log("Error occured: ", error));
+  };
 
-  componentDidMount(){
-    return this.fetchData()
+  componentDidMount() {
+    this.fetchData();
   }
 
-  render() {    
+  render() {
+    let users = this.state.users;
+    console.log(this.state.users)
+    if (users.length === 0) {
+      return <p>loading...please wait...</p>
+    }
+  
     return (
       <div>
-        <h1>Rolodex</h1>
-        <UserCard 
-          users={this.state.users.results}
-        />
+        <h1>rolodex</h1>
       </div>
     );
   }
